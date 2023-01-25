137f0a7eb7192fa33d3d59ca517dd59a57d3ac8a
DexBattle <roddx2021@gmail.com>
2023-01-25 13:15:06 +0400

    css fix

--- pages/Comics.js
+++ pages/Comics.js
@@ -6,7 +6,7 @@ export default function Comics(){
             <div  className='flex flex-col items-center md:mx-16 mx-4 overflow-hidden'>
             <h1 className='font-Kanit text-2xl text-[66px] text-blue-400 mt-[100px] mb-4'>Comics</h1>
             <div className='w-full h-full flex flex-col items-center my-4'>
-                <img src='/comics/Cover.jpg' className='w-[800px] h-[950px]'/>
+                <img src='/comics/Cover.jpg' className='w-[800px] h-[970px] rounded-sm border border-white'/>
             </div>
             <div className='grid grid-flow-row md:grid-cols-2 my-4'>
                 <img src='/comics/01.jpg' className='rounded-md h-full-w-full mx-2 my-2'/>
