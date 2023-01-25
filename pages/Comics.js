4c065238db3a31484bd60a2d54ea80c03ce1c0dc
DexBattle <roddx2021@gmail.com>
2023-01-25 12:53:06 +0400

    space fix

--- pages/Comics.js
+++ pages/Comics.js
@@ -5,7 +5,7 @@ export default function Comics(){
             <Navbar/>
             <div  className='flex flex-col items-center md:mx-16 mx-4 overflow-hidden'>
             <h1 className='font-Kanit text-2xl text-[60px] text-blue-400 mt-[100px] mb-4'>Comics</h1>
-            <div className='grid grid-flow-row md:grid-cols-2 space-6'>
+            <div className='grid grid-flow-row md:grid-cols-2 space-x-6 space-y-6 my-4'>
                 <img src='/comics/01.jpg' className='object-cover rounded-md h-full-w-full'/>
                 <img src='/comics/02.jpg' className='object-cover rounded-md h-full-w-full'/>
                 <img src='/comics/03.jpg' className='object-cover rounded-md h-full-w-full'/>
