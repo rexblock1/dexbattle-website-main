7269de2701b13ffd75136f9de34c2dcb4bd90bf8
DexBattle <roddx2021@gmail.com>
2023-01-27 10:58:06 +0400

    css fix

--- pages/tournament.js
+++ pages/tournament.js
@@ -6,7 +6,7 @@ export default function Tournament ( ) {
     return (
         <div className='overflow-y-hidden'>
           <Navbar/>
-          <div className='w-screen h-full lg:mx-16 mx-4 flex flex-col'>
+          <div className='h-full lg:mx-16 mx-4 flex flex-col items-center'>
             <div className='mt-[100px] mb-8'>
                 <ReactPlayer url='https://youtu.be/P19JkM2k2hI'
                  controls={true} muted playing
